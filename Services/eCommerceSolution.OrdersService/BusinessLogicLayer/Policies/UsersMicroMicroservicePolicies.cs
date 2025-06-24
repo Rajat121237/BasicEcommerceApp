0b793044581b4233df2fccb0a234b2c4e2429b03
Rajat Kumar Pandey <rajat121237@gmail.com>
2025-06-24 07:47:14 +0530

    Applied exponential backoff strategy

--- Services/eCommerceSolution.OrdersService/BusinessLogicLayer/Policies/UsersMicroMicroservicePolicies.cs
+++ Services/eCommerceSolution.OrdersService/BusinessLogicLayer/Policies/UsersMicroMicroservicePolicies.cs
@@ -14,7 +14,7 @@ public class UsersMicroMicroservicePolicies : IUsersMicroMicroservicePolicies
     public IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
     {
         AsyncRetryPolicy<HttpResponseMessage>  retryPolicy = Policy.HandleResult<HttpResponseMessage>(response => !response.IsSuccessStatusCode)
-            .WaitAndRetryAsync(retryCount: 5, sleepDurationProvider: retryAttempt => TimeSpan.FromSeconds(2),
+            .WaitAndRetryAsync(retryCount: 5, sleepDurationProvider: retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
             onRetry: (outcome, timespan, retryCount, context) =>
             {
                 _logger.LogInformation($"Retry {retryCount} due to {outcome.Result.StatusCode} at {DateTime.UtcNow}. Retrying in {timespan.TotalSeconds} seconds.");
